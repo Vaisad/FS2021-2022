ea47ab71e0470c8c049d5abb61a4a49fc3c4f1cb
Vaidas Sauklys <vaidass08@gmail.com>
2021-11-04 17:48:06 +0100

    JS

--- javaScript/2021.11.04/script.js
+++ javaScript/2021.11.04/script.js
@@ -1,8 +1,8 @@
 let cars = [
-    new Cars('Porsche', 'Cayenne', 2009),
-    new Cars('Mercedes-Benz', 'GLE', 2020),
-]; 
+	new Cars('Porsche', 'Cayenne', 2009),
+	new Cars('Mercedes-Benz', 'GLE', 2020),
+];
 
-for(let a of cars){
-    console.log(`${a.getIntroduction()} - ${a.getAge()} `);
+for (let a of cars) {
+	console.log(`${a.getIntroduction()} - ${a.getAge()} `);
 };
