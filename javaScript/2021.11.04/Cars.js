1332ab9ee990a00111db33a4f3a005b492dc23f0
Vaidas Sauklys <vaidass08@gmail.com>
2021-11-04 17:51:23 +0100

    JS

--- javaScript/2021.11.04/Cars.js
+++ javaScript/2021.11.04/Cars.js
@@ -1,15 +1,15 @@
 
 class Cars {
-	constructor(make, model, year) {
-		this.make = make;
-		this.model = model;
-		this.year = year;
-	};
-	getIntroduction() {
-		return `${this.make} ${this.model}`;
-	};
-	getAge() {
-		let age = (new Date).getFullYear() - this.year;
-		return age <= 10 ? '10 years or later' : '11 years or older';
-	}
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    };
+    getIntroduction() {
+        return `${this.make} ${this.model}`;
+    };
+    getAge() {
+        let age = (new Date).getFullYear() - this.year;
+        return age <= 10 ? '10 years or later' : '11 years or older';
+    }
 };
